451064179e73ac0d81c206dff2be7103bb08f926
Oleh Tymoshchuk <pryge321@gmail.com>
2025-04-07 18:18:16 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -22,29 +22,37 @@ export class App extends Component<{}, State> {
 
   timerId: number = 0;
 
-  timeId: number = 0;
-
   startClock = () => {
-    this.timerId = window.setInterval(() => {
-      this.setState({ clockName: getRandomName() });
-    }, 3300);
+    // Clear any existing timer before starting a new one
+    if (this.timerId) {
+      clearInterval(this.timerId);
+    }
 
-    this.timeId = window.setInterval(() => {
+    // Set an interval to update the time and clock name
+    this.timerId = window.setInterval(() => {
       const newTime = new Date();
 
-      // eslint-disable-next-line no-console
+      // Update clock name every 3300ms
+      this.setState({
+        clockName: getRandomName(),
+        today: newTime,
+      });
+
+      // Log time to console every second and update the displayed time
       if (this.state.hasClock) {
         // eslint-disable-next-line no-console
         console.log(newTime.toUTCString().slice(-12, -4));
       }
+    }, 1000); // Time update every second
 
-      this.setState({ today: newTime });
-    }, 1000);
+    // This timer will update the clock name more accurately every 3300ms
+    window.setInterval(() => {
+      this.setState({ clockName: getRandomName() });
+    }, 3300); // Update clock name every 3300ms
   };
 
   stopClock = () => {
     clearInterval(this.timerId);
-    clearInterval(this.timeId);
   };
 
   handleShowClock = () => {
@@ -66,13 +74,16 @@ export class App extends Component<{}, State> {
   componentDidUpdate(_: {}, prevState: State) {
     if (!prevState.hasClock && this.state.hasClock) {
       this.startClock();
+      // Оновлюємо час негайно при показі годинника
+      this.setState({ today: new Date() });
     }
 
     if (prevState.hasClock && !this.state.hasClock) {
       this.stopClock();
     }
 
-    if (prevState.clockName !== this.state.clockName) {
+    // Логування буде тільки якщо ім'я дійсно змінилося
+    if (prevState.clockName !== this.state.clockName && this.state.hasClock) {
       // eslint-disable-next-line no-console
       console.warn(
         `Renamed from ${prevState.clockName} to ${this.state.clockName}`,
@@ -82,7 +93,6 @@ export class App extends Component<{}, State> {
 
   componentWillUnmount() {
     clearInterval(this.timerId);
-    clearInterval(this.timeId);
 
     document.removeEventListener('click', this.handleShowClock);
     document.removeEventListener('contextmenu', this.handleHideClock);
@@ -91,14 +101,12 @@ export class App extends Component<{}, State> {
   render() {
     return (
       <div className="App">
-        <h1>React clock</h1>
+        <h1>React Clock</h1>
 
         {this.state.hasClock && (
           <div className="Clock">
             <strong className="Clock__name">{this.state.clockName}</strong>
-
             {' time is '}
-
             <span className="Clock__time">
               {this.state.today.toUTCString().slice(-12, -4)}
             </span>
